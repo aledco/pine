b4b65b1d28371e21a17adfa44075150fc313ab33
Alexander Cooper <alexanderecooper@gmail.com>
2024-11-22 07:01:39 -0700

    save

--- app/Scan.hs
+++ app/Scan.hs
@@ -1,6 +1,17 @@
 module Scan where
 
+import Data.Char
 
-scan :: String -> IO ()
-scan input = putStrLn input
+
+scan :: String -> [Char]
+scan input = scan' input 1 1
+    where
+        scan' [] l c = []
+        scan' (' ':cs) l c = scan' cs l c+1
+        scan' ('\n':cs) l c = scan' cs l+1 1      
+        scan' (c:cs) line col 
+            | isAlpha c = scanIdOrKey c:cs l c
+            | otherwise = [c]
+            where
+                scanIdOrKey s l c = []   
 
